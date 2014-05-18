3e2aac5423eff39874675f0093b9c10f61276469
Sid Tantia <sstantia@gmail.com>
2014-05-18 15:44:44 -0400

    working ajax create

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,20 +1,19 @@
-// $(function(){
+$(function(){
 
-// 	$('#add').click(function() {
-// 		$('.new-task').fadeIn('fast');
-// 		console.log('Hello')
-
-// 	});
+	$('#add').click(function() {
+		$('.new-task').fadeIn('fast');
+		console.log('Hello')
+	});
 
 	
 
-// 	$('#closer').click(function() {
-// 		$('.new-task').fadeOut('slow');
-// 	});
+	$('#closer').click(function() {
+		$('.new-task').fadeOut('slow');
+	});
 
 // // 	jQuery('#datetimepicker').datetimepicker();
 
-// });
+});
 
 // // oncreate instead of hiding modal OR create partial for creating modal
 
